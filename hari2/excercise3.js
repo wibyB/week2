642a179f5262a432858d01340543c2e91a8c6cb7
wiby <wibybramantyo123@gmail.com>
2020-03-25 19:13:31 +0700

    revisi exercise3

--- hari2/excercise3.js
+++ hari2/excercise3.js
@@ -1,24 +1,24 @@
 var nama = 'Bolky'
-var peran = 'Ksatria' 
+var peran = '' 
 
-if (nama === '' || peran === '') {
-    console.log('nama harus di isi')
+if (!nama && !peran) {
+    console.log('atribut harus di isi')
 }
-else if(nama === "Bolky" && peran === '') {
+else if(nama && !peran) {
     console.log('Hallo'+ ' ' + nama + ' ' + 'Pilih peranmu untuk memulai game') 
 }
-else if(nama === 'Bolky' && peran === 'Ksatria'){
+else if(nama && peran === 'ksatria'){
     console.log('Selamat datang di Dunia Proxytia, ' + nama)
     console.log('Halo ' +  peran + ' ' + nama +',' + ' kamu dapat menyerang dengan senjatamu!' )
 }
-else if(nama === 'Bolky' && peran === 'Tabib'){
+else if(nama && peran === 'Tabib'){
     console.log('Selamat datang di Dunia Proxytia, ' + nama)
     console.log('Halo ' +  peran + ' ' + nama +',' + ' kamu akan membantu temanmu yang terluka.' )
 }
-else if(nama === 'Bolky' && peran === 'Penyihir'){
+else if(nama && peran === 'Penyihir'){
     console.log('Selamat datang di Dunia Proxytia, ' + nama)
     console.log('Halo ' +  peran + ' ' + nama +',' + ' ciptakan keajaiban yang membantu kemenanganmu!' )
 }
 else {
-    console.log('Pilih peranmu untuk memulai game')
+    console.log('default')
 }
