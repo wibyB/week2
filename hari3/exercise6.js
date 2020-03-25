6d8ddabc03a315ca5ea3a08e839be5ade476edb7
wiby <wibybramantyo123@gmail.com>
2020-03-25 18:54:56 +0700

    exercise 6 + 7

---
+++ hari3/exercise6.js
@@ -0,0 +1,91 @@
+//nomer 1
+var a = 0
+var b = 22
+
+console.log('LOOPING PERTAMA')
+while (a < 20){
+    a = a + 2
+    console.log(a + ' -I Love Coding')
+}
+console.log('LOOPING KE DUA')
+while (b > 0){
+    b = b - 2
+    console.log(b + '- I WILL BECOME FULLSTACK DEVELOPER')
+}
+
+//nomer 2
+var c = 0
+var d = 21
+
+console.log('LOOPING PERTAMA')
+while (c < 20){
+    c++
+    console.log(c + ' -I Love Coding')
+}
+console.log('LOOPING KE DUA')
+while (d > 0){
+    d--
+    console.log(d + '- I WILL BECOME FULLSTACK DEVELOPER')
+}
+
+// nomer 3
+var e = 0
+
+console.log('ganjil - genap')
+while (e < 100){
+    e++
+    if(e % 2 == 0){
+        console.log("counter sekarang " + e)
+        console.log(`${'output'}
+        ${'genap'}`)
+    }else{
+        console.log("counter sekarang " + e)
+        console.log(`${'output'}
+        ${'ganjil'}`)
+    }
+}
+
+var f = 1
+while (f < 100){
+    f = f + 2
+    if (f % 3 == 0){
+        console.log('counter sekarang ' + f)
+        console.log(`${'output'}
+        ${f + " kelipatan 3"}`)
+    }else{
+        console.log('counter sekarang ' + f)
+        console.log(`${'output'}
+        ${"''"}`)
+    }
+
+}   
+
+var g = 1
+while (g < 100){
+    g = g + 5
+    if (g % 6 == 0){
+        console.log('counter sekarang ' + g)
+        console.log(`${'output'}
+        ${g + " kelipatan 6"}`)
+    }else{
+        console.log('counter sekarang ' + g)
+        console.log(`${'output'}
+        ${"''"}`)
+    }
+
+}   
+
+var h = 1
+while (h < 100){
+    h = h + 9
+    if (h % 10 == 0){
+        console.log('counter sekarang ' + h)
+        console.log(`${'output'}
+        ${h + " kelipatan 10"}`)
+    }else{
+        console.log('counter sekarang ' + h)
+        console.log(`${'output'}
+        ${"''"}`)
+    }
+
+}   
---
+++ hari3/exercise7.js
@@ -0,0 +1,38 @@
+var rows1 = 5 // input the number of rowss
+var i = 0
+
+
+
+// do loops to display asterisks in the console.
+while (i < rows1){
+    i++
+    console.log('*')
+}
+
+
+var rows2 = 5 // input the number of rows
+var col2 = 5
+
+
+// do loops to display asterisks in the console.
+for (a = 0; a < rows2; a++ ){
+    var c = ""
+    for (b = 0; b < col2; b++){
+        c = c + "*"
+    }
+    console.log(c)
+
+}
+
+
+// nomer 3
+var rows3 = 5 // input the number of rows
+
+// do loops to display asterisks in the console.
+for ( d = 0; d <= rows3; d++){
+    var f = ""
+    for (e = 0; e < d; e++){
+        f = f + "*"
+    }
+    console.log(f)
+}
